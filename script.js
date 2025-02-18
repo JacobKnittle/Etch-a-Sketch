6b449d0f02769b4d0ba2c97efc21ab15b01286c1
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-18 10:50:41 -0800

    converted grid loop to a function

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 // container div
 const container = document.querySelector("#container");
 
-//16x16 grid of square divs
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let square = document.createElement("div");
-    square.className = "square";
-    container.appendChild(square);
+//creates grid of square divs
+function createGrid(size) {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      let square = document.createElement("div");
+      square.className = "square";
+      container.appendChild(square);
+    }
   }
 }
+createGrid(16);
 
-// adds a grey background color to a div 
+// adds a grey background color to a div
 container.addEventListener("mouseover", (e) => {
   e.target.classList.add("fillIn");
   console.log(e.target);
 });
-
 
 // add a button to the top that sends a popup for user input of grid size with a 100 limit
 
