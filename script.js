725a6f32e441a6deb30e33d9ad80ae3243c896f3
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-18 10:45:39 -0800

    add grey background to squares when mouse hovers over them

--- script.js
+++ script.js
@@ -10,8 +10,17 @@ for (let i = 0; i < 16; i++) {
   }
 }
 
-container.addEventListener("mouseenter", (e) => {
-  let target = e.target;
-  console.log(target);
-  //   target.style.backgroundColor = "gray";
+// adds a grey background color to a div 
+container.addEventListener("mouseover", (e) => {
+  e.target.classList.add("fillIn");
+  console.log(e.target);
 });
+
+
+// add a button to the top that sends a popup for user input of grid size with a 100 limit
+
+// button that runs the grid creation function
+
+// remove existing grid
+
+// general the new grid in the same total space
